cb27de187ad1779d4655a3168ff5d02f5aa08ee0
joaopscalazans <joaopscalzans@gmail.com>
2026-09-07 15:17:33 -0300

    cria entidade Vendedor e sua relacao 1:1 com usuario

---
+++ E-commerce/Entity/Model/Vendedor.cs
@@ -0,0 +1,25 @@
+namespace E_commerce.Entity.Model;
+
+public class Vendedor
+{
+
+    public int Id { get; private set; }
+    public int IdUsuario { get; private set; }
+    public Usuario Usuario { get; set; }
+    public string? Cnpj { get; set; }
+    public string? Cpf { get; set; }
+    public string NomeLoja { get; set; }
+    
+    public Vendedor()
+    {}
+
+    public Vendedor(Usuario usuario, string cnpj, string cpf, string nomeLoja)
+    {
+        Id = usuario.Id;
+        IdUsuario = usuario.Id;
+        Usuario = usuario;
+        Cnpj = cnpj;
+        Cpf = cpf;
+        NomeLoja = nomeLoja;
+    }
+}
